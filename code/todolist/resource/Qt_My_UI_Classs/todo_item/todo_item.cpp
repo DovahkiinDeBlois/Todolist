1463c00659e41e0f092c1ab50d2156ac07f7cf00
kamijin <1411905376@qq.com>
2024-01-19 00:42:30 +0800

    添加日历（这个日历怎么只显示表头啊？)

--- code/todolist/resource/Qt_My_UI_Classs/todo_item/todo_item.cpp
+++ code/todolist/resource/Qt_My_UI_Classs/todo_item/todo_item.cpp
@@ -6,9 +6,29 @@ todo_item::todo_item(QWidget *parent) :
     ui(new Ui::todo_item)
 {
     ui->setupUi(this);
+    this->scs_init();
 }
 
 todo_item::~todo_item()
 {
     delete ui;
+}
+
+void todo_item::scs_init(){
+    connect(this->ui->btn_date, &QPushButton::clicked, this, &todo_item::show_date);
+}
+
+void todo_item::show_date(){
+    if (!show_date_flag){
+        choose_date = new QCalendarWidget(this);
+        choose_date->show();
+        choose_date->resize(256,560);
+        QPoint btm_date_pos = this->ui->btn_date->pos();
+        QSize btm_date_size = this->ui->btn_date->size();
+        QPoint win_date_pos = this->ui->win_date->pos();
+        QSize win_date_size = this->ui->win_date->size();
+
+        choose_date->move(btm_date_pos.x() + win_date_pos.x(), btm_date_pos.y()+btm_date_size.height()+win_date_pos.y());
+        show_date_flag = true;
+    }
 }
--- code/todolist/resource/Qt_My_UI_Classs/todo_item/todo_item.h
+++ code/todolist/resource/Qt_My_UI_Classs/todo_item/todo_item.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QSize>
+# include <QCalendarWidget>
 
 # include "./../../../basic_definition/basic_definition.h"
 
@@ -17,10 +18,20 @@ class todo_item : public QWidget
 public:
     explicit todo_item(QWidget *parent = 0);
     // void set_wh(QSize wiget_size_list[todo_item_MAX]);
+
+
     ~todo_item();
 
 private:
     Ui::todo_item *ui;
+    // 链接槽函数的函数
+    void scs_init();
+    bool show_date_flag = false; // 判断当前是否有显示可供选择的日历
+
+    // 日历变量，使用前请判断是否是空指针，不使用的时候设置为空指针
+    QCalendarWidget * choose_date = nullptr;
+    // 显示日历并选择
+    void show_date();
 };
 
 #endif // TODO_ITEM_H
